095ca642ad81e80fbeadf8493d5ce343d5904ce9
eholtam <(none)>
2020-10-22 08:18:20 -0500

    Fixed migration drops, merged client script run into one function.

--- migrations/2020_10_21_000001_filevault_status_add_bootstrap_columns.php
+++ migrations/2020_10_21_000001_filevault_status_add_bootstrap_columns.php
@@ -29,27 +29,6 @@ class FilevaultStatusAddBootstrapColumns extends Migration
     {
         $capsule = new Capsule();
         $capsule::schema()->table($this->tableName, function (Blueprint $table) {
-            $table->dropColumn('auth_restart_support');
-            $table->dropColumn('fv_master_keychain');
-            $table->dropColumn('has_institutional_recovery_key');
-            $table->dropColumn('has_personal_recovery_key');
-            $table->dropColumn('using_recovery_key');
-            $table->dropColumn('conversion_percent');
-            $table->dropColumn('bytes_converted');
-            $table->dropColumn('volume_size');
-            $table->dropColumn('conversion_state_detail');
-            $table->dropColumn('conversion_state');
-            $table->dropColumn('fv_progress_status');
-            $table->dropColumn('device_identifier');
-            $table->dropColumn('pvdeviceid');
-            $table->dropColumn('volume_name');
-            $table->dropColumn('lvf_uuid');
-            $table->dropColumn('lvg_uuid');
-            $table->dropColumn('pv_uuid');
-            $table->dropColumn('uuid');
-            $table->dropColumn('filevault_users');
-            $table->dropColumn('crypto_users');
-            $table->dropColumn('deferral_info');
             $table->dropColumn('bootstraptoken_supported');
             $table->dropColumn('bootstraptoken_escrowed');
        });
