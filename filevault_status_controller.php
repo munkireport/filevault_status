52cf9b01ebdbdf79afe4b5c62f39209c9e8deb2c
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:39:54 -0400

    Update filevault_status_controller.php

--- filevault_status_controller.php
+++ filevault_status_controller.php
@@ -186,6 +186,9 @@ class Filevault_status_controller extends Module_controller
      **/
     public function get_status_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
 
         if (! $this->authorized()) {
